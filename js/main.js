82cc9e7d7a497aea856f0a90a4c99d7a18766407
SalahudinMalik <salahudiniiui@gmail.com>
2018-04-26 13:26:14 -0700

    data img

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Salahudin Malik.", "Programmer.", "Developer.", "Freelancer.", "Data Scientist."],
       typeSpeed: 100,
       loop: true,
     });
